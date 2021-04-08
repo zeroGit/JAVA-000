0904363820c1427f9673138ed502593f5d663005
zhangmeng01 <zhangmeng01@17guagua.com>
2021-04-08 18:35:42 +0800

    x

---
+++ Week_06/4_1a2/README.md
@@ -0,0 +1,7 @@
+
+
+1.（选做）尝试使用 Lambda/Stream/Guava/Lombok 优化之前作业的代码。
+
+2.（选做）尝试使用 Lambda/Stream/Guava/Lombok 优化工作中编码的代码。
+
+
---
+++ Week_06/4_1a2/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Guava 东西挺多，很多类，很多方法，看不下去了，用的时候找找看看吧，jdk都看不完了");
+    }
+}
